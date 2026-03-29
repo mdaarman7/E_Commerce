6483ceeb464aa709e1f5e4318da5f3c3858a4c21
Mohammad Aarman <inn.mdaarman@gmail.com>
2026-03-29 12:49:15 +0545

    Icons Updated for Edit and Delete

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -26,28 +26,65 @@
                     <td class="border px-4 py-2">{{ $product->stock }}</td>
                     <td class="border px-4 py-2">
                         @if($product->image)
-                            <img src="{{ asset('storage/'.$product->image) }}" alt="{{ $product->name }}" class="w-16 h-16 object-cover">
+                        <img src="{{ asset('storage/'.$product->image) }}" alt="{{ $product->name }}" class="w-16 h-16 object-cover">
                         @else
-                            N/A
+                        N/A
                         @endif
                     </td>
                     <td class="border px-4 py-2">
                         <div class="flex gap-2 items-center">
-                            <a href="{{ route('products.edit', $product->id) }}" class="bg-yellow-400 text-white px-3 py-2 rounded hover:bg-yellow-500 transition" title="Edit">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+
+                            <!-- Edit Button -->
+                            <a href="{{ route('products.edit', $product->id) }}"
+                                class="bg-yellow-400 text-white p-2 rounded hover:bg-yellow-500 transition flex items-center justify-center"
+                                title="Edit">
+
+                                <!-- Edit SVG Icon -->
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    class="w-5 h-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor">
+
+                                    <path stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M15.232 5.232l3.536 3.536M9 13l6.768-6.768a2.5 2.5 0 013.536 3.536L12.536 16.536a4 4 0 01-1.414.943L8 18l.521-3.121A4 4 0 019.464 13z" />
                                 </svg>
+
                             </a>
 
-                            <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="inline-block">
+
+                            <!-- Delete Button -->
+                            <form action="{{ route('products.destroy', $product->id) }}"
+                                method="POST"
+                                class="inline-block">
+
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600 transition" title="Delete" onclick="return confirm('Are you sure you want to delete this product?')">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+
+                                <button type="submit"
+                                    class="bg-red-500 text-white p-2 rounded hover:bg-red-600 transition flex items-center justify-center"
+                                    title="Delete"
+                                    onclick="return confirm('Are you sure you want to delete this product?')">
+
+                                    <!-- Delete SVG Icon -->
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="w-5 h-5"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor">
+
+                                        <path stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M6 7h12M9 7V4h6v3M10 11v6M14 11v6M5 7l1 13a2 2 0 002 2h8a2 2 0 002-2l1-13" />
                                     </svg>
+
                                 </button>
+
                             </form>
+
                         </div>
                     </td>
                 </tr>
