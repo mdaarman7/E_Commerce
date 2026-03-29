0a9e47bbe38632ed4b342c7bf9c96007a61eb17d
Mohammad Aarman <inn.mdaarman@gmail.com>
2026-03-29 14:01:30 +0545

    middleware added for roles

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, string $role): Response
+    {
+        // if user not logged in
+        if (!$request->user()) {
+            return redirect('/login');
+        }
+
+        // if role does not match
+        if ($request->user()->role !== $role) {
+            abort(403, 'Unauthorized access');
+        }
+
+        return $next($request);
+    }
+}
