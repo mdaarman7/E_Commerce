3c4da531dd4204fda5dae354609e7ad87b9fa8c0
Mohammad Aarman <inn.mdaarman@gmail.com>
2026-04-16 22:32:06 +0545

    Total Cost of the cart's amount Displayed

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
-
     <x-slot name="header">
-
         <h2>My Cart</h2>
-
     </x-slot>
 
     <div class="p-10">
+        @php
+            $total = 0;
+        @endphp
 
         @foreach($carts as $cart)
+        @php
+            $total += $cart->product->price * $cart->quantity;
+        @endphp
+
         <div class="flex gap-5 items-center border p-3 mb-3">
             <img
                 src="{{ asset('storage/'.$cart->product->image) }}"
@@ -34,8 +38,16 @@
                     Remove
                 </button>
             </form>
+
         </div>
         @endforeach
 
+        <div class="text-right">
+            @if ($carts->count() > 0)
+                <h3 class="text-xl font-bold border p-3 inline-block">
+                Total: Rs {{ $total }}
+                </h3>
+            @endif
+        </div>
     </div>
 </x-app-layout>
